719cb4ff79602727a15042b80b2d56205e2828ee
Work <mihai.cristescu@gmail.com>
2014-02-27 12:56:48 +0200

    oferte

---
+++ php/temp.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'conexiune.php';
+?>
+<form action="/" method="post" id="selectie_persoana">
+    <label for="select_companie">Companie</label>
+    <input id="select_companie"
+           name="select_companie"
+           type="text"
+           class="lung normal"
+           value=""
+           data-id=""
+           placeholder="Tastează pentru a căuta..."/>
+    <br/><br/>
+    <label for="select_persoana">Persoană de contact</label>
+    <input id="select_persoana"
+           name="select_persoana"
+           type="text"
+           class="normal scurt"
+           value=""
+           data-id=""
+           placeholder="Selectează..."
+           readonly
+           />
+    <br/><br/>
+    <span id="word" class="submit">Generează ofertă</span>
+</form>
+<div id="lista_persoane" class="ddm"></div>
+<div id="lista_companii" class="ddm"></div>
